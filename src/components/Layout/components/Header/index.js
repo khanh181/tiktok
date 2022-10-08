7751a1f88bc38d5cd3974fd079aa939af420c5f6
khanh181 <91577673+khanh181@users.noreply.github.com>
2022-10-08 11:12:57 +0700

    chua xong

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,12 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/imgs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleXmark,
+  faSpinner,
+  faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
 
 //! giups bind cái obj styles rồi trả ra med là 1 function là cái cx và dùng cx để dùng class, và tên nó tự biến thành -
 const cx = classNames.bind(styles);
@@ -8,8 +15,24 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        {/* logo */}
-        {/* search */}
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx('search')}>
+          <input placeholder="Search video and account..." spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+            <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          </button>
+          <span></span>
+          <button className={cx('search-btn')}>
+            <FontAwesomeIcon
+              className={cx('loading')}
+              icon={faMagnifyingGlass}
+            />
+          </button>
+        </div>
+        <div className={cx('actions')}></div>
       </div>
     </header>
   );
